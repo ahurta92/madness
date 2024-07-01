c9e86d26db06e970e82425cb5d0b4a1a0ee947fd
ahurta92 <ahurta92@gmail.com>
2024-07-01 16:41:52 -0400

    Revert "QCPropertyInterface.h, included thorughout chem headers, imported namespace madness into global space which amazingly did not break anything until recently"
    This reverts commit bc06d92c3fc0d1bfa236c12d30b85856259b5bd7.

--- src/madness/chem/SCF.cc
+++ src/madness/chem/SCF.cc
@@ -36,9 +36,9 @@
 
 
 #include <madness/world/worldmem.h>
-#include <madness.h>
-#include <madness/chem/SCF.h>
-#include <madchem.h>
+#include<madness.h>
+#include<madness/chem/SCF.h>
+#include<madchem.h>
 
 #if defined(__has_include)
 #  if __has_include(<filesystem>)
@@ -211,7 +211,7 @@ scf_data::scf_data() : iter(0) {
 
 
 void scf_data::to_json(json &j) const {
-    madness::print("SCF DATA TO JSON");
+    ::print("SCF DATA TO JSON");
 
     j["scf_e_data"] = json();
     j["scf_e_data"]["iterations"] = iter;
@@ -2370,4 +2370,3 @@ void SCF::solve(World& world) {
 
 }        // end solve function
 
-} // namespace madness
